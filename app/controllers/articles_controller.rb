e2a89ab10ecd89c339cdbcd96fc304856a2daa2a
yerwansyah <yuzzr.rizal@gmail.com>
2017-09-24 23:33:36 +0700

    Partial Page and Delete Article:
    * Partial Page and Delete Article:
    * Add destroy action method
    * Add _form partial page
    * Use _form partial page
    * Add link_to delete action
    * Add _messages partial page
    * Use _messages partial page
    * Add link to Blog Routes from home
    * extract redudancy for finding an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
   def index
     @articles = Article.all
   end
@@ -9,7 +11,7 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+
   end
 
   def create
@@ -27,8 +29,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -41,10 +41,26 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+
+  end
+
+  def destroy
+    if @article.destroy
+      flash[:notice] = "Article was successfully deleted"
+    else
+      flash[:notice] = "Article can't be deleted"
+    end
+    redirect_to articles_path
+  rescue StandardError
+    flash[:notice] = "Error when deleting article"
+    redirect_to articles_path
   end
 
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :description)
